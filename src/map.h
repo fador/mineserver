e67f5c87541509fa086117f8d8fb3097caaa450f
Tobias Pape <tobias@netshed.de>
2010-11-12 18:11:56 +0100

    fix compile on mac

--- src/map.h
+++ src/map.h
@@ -25,8 +25,8 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifndef _MAP_H
-#define _MAP_H
+#ifndef _MAP_H_
+#define _MAP_H_
 
 #include <map>
 #include <ctime>
