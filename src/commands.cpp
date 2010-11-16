fador/mineserver

commit 5859e549b3e66220f28614202f7f62df62996b77

Little tweaks

src/commands.cpp
@@ -52,7 +52,7 @@ namespace
 
 void reportError(User *user, std::string message)
 {
-  Chat::get().sendMsg(user, COLOR_DARK_MAGENTA + "Error!" + COLOR_RED + message, Chat::USER);
+  Chat::get().sendMsg(user, COLOR_DARK_MAGENTA + "Error! " + COLOR_RED + message, Chat::USER);
 }
 
 void playerList(User *user, std::string command, std::deque<std::string> args)
@@ -267,7 +267,10 @@ void giveItems(User *user, std::string command, std::deque<std::string> args)
 
     // Check item validity
     if(!isValidItem(itemId))
+    {
+      reportError(user, "Item " + args[1] + " not found.");
       return;
+    }
 
     if(args.size() > 2)
     {
@@ -316,7 +319,7 @@ void giveItems(User *user, std::string command, std::deque<std::string> args)
 }
 
 void Chat::registerStandardCommands()
-{
+{wha
   registerCommand("players", playerList, false);
   registerCommand("about", about, false);
   registerCommand("rules", rules, false);

src/nbt.cpp
@@ -463,7 +463,7 @@ int dumpNBT_list(NBT_list *input, uint8 *buffer)
 
   putSint32(&buffer[curpos], input->length);
   curpos += 4;
-  NBT_struct **structlist=(NBT_struct **)input->items;
+  //NBT_struct **structlist=(NBT_struct **)input->items;
   for(int i = 0; i < input->length; i++)
   {
     switch(input->tagId)

commit e7d71e964bd57309aa84fd1acc9ec13d406b1c38

Fixed a compile error introduced in 5859e549.
Signed-off-by: Alastair Lynn <arplynn@gmail.com>

src/commands.cpp
@@ -319,7 +319,7 @@ void giveItems(User *user, std::string command, std::deque<std::string> args)
 }
 
 void Chat::registerStandardCommands()
-{wha
+{
   registerCommand("players", playerList, false);
   registerCommand("about", about, false);
   registerCommand("rules", rules, false);