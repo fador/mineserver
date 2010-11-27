811308881df25ffbc8aa7760fc914c0ec713bb2c
Fador <fador@iki.fi>
2010-11-27 23:40:37 +0200

    changed netdb.h to only be loaded on linux

--- src/mineserver.cpp
+++ src/mineserver.cpp
@@ -37,6 +37,7 @@
   #include <netinet/in.h>
   #include <arpa/inet.h>
   #include <string.h>
+  #include <netdb.h>
 #endif
 
 #include <sys/types.h>
@@ -67,7 +68,6 @@
 #include "packets.h"
 #include "physics.h"
 
-#include <netdb.h>
 
 #ifdef WIN32
 static bool quit = false;
