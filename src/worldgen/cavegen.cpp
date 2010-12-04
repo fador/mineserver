7bf065e70c841093827f735ad68739dab197304a
Fador <fador@iki.fi>
2010-12-04 19:28:04 +0200

    cave_lava and cave_ore settings actually do something now

--- src/worldgen/cavegen.cpp
+++ src/worldgen/cavegen.cpp
@@ -91,7 +91,7 @@ void CaveGen::AddCaves(uint8 &block, double x, double y, double z)
     {
       
       // Add bottomlava
-      if(y < 10.0)
+      if(y < 10.0 && addCaveLava)
       {
         block = BLOCK_STATIONARY_LAVA;
         return;
@@ -102,7 +102,7 @@ void CaveGen::AddCaves(uint8 &block, double x, double y, double z)
     }
 
     
-    if(y < 60)
+    if(y < 60 && addOre)
     {      
       caveN1 = caveNoise1.GetValue(x,y,z);
       if(caveN1 > 0.56)
