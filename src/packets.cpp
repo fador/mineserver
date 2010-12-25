ec9d0d734df598fa276b5eef1afc06bee108681f
Fador <fador@iki.fi>
2010-12-25 03:02:11 +0200

    sign placement now actually saves the sign data (but not with the map data yet!)

--- src/packets.cpp
+++ src/packets.cpp
@@ -115,6 +115,29 @@ int PacketHandler::change_sign(User *user)
     user->buffer >> strings[i];
   }
   //ToDo: Save signs!
+  signData *newSign = new signData;
+  newSign->x = x;
+  newSign->y = y;
+  newSign->z = z;
+  newSign->text[0] = strings[0];
+  newSign->text[1] = strings[1];
+  newSign->text[2] = strings[2];
+  newSign->text[3] = strings[3];
+
+  sChunk* chunk = Mineserver::get()->map()->chunks.GetChunk(x,z);
+
+  if(chunk != NULL)
+  {
+    chunk->signs.push_back(newSign);
+
+    //Send sign packet to everyone
+    Packet pkt;
+    pkt << (sint8)PACKET_SIGN << x << y << z;
+    pkt << strings[0] << strings[1] << strings[2] << strings[3];
+
+    user->sendAll((uint8 *)pkt.getWrite(), pkt.getWriteLen());
+
+  }
 
   Mineserver::get()->screen()->log(LOG_GENERAL,"Sign: " + strings[0] + strings[1]+ strings[2]+ strings[3]);
   
