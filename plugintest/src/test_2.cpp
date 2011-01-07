89ee7bb5e49648cfd2980361a949d719ed2bffc3
deoxxa <deoxxa@fknsrs.biz>
2011-01-07 11:47:28 +0900

    fixed log statements

--- plugintest/src/test_2.cpp
+++ plugintest/src/test_2.cpp
@@ -1,8 +1,9 @@
 #include "../../src/plugin_api.h"
+#include "../../src/logtype.h"
 
 PLUGIN_API_EXPORT void test_2_init(mineserver_pointer_struct* mineserver)
 {
-  mineserver->logger.log("test_2_init");
+  mineserver->logger.log(LogType::LOG_INFO, "plugin.test_2", "test_2_init");
 }
 
 PLUGIN_API_EXPORT void test_2_shutdown(void)
--- plugintest/src/test_3.cpp
+++ plugintest/src/test_3.cpp
@@ -1,9 +1,10 @@
 #include "../../src/plugin_api.h"
+#include "../../src/logtype.h"
 #include "../../src/hook.h"
 
 PLUGIN_API_EXPORT void test_3_init(mineserver_pointer_struct* mineserver)
 {
-  mineserver->logger.log("test_3_init");
+  mineserver->logger.log(LogType::LOG_INFO, "plugin.test_2", "test_3_init");
   mineserver->plugin.setHook("test", new Hook0<void>);
 }
 
