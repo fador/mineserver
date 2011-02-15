e912cd7b0bc3915d5df1cb917cc271221e7dc48a
Satome <mika.alasippola@saunalahti.fi>
2011-02-16 00:45:07 +0200

    Cake

--- src/blocks/cake.h
+++ src/blocks/cake.h
@@ -39,5 +39,9 @@ class BlockCake: public BlockBasic
 public:
   bool affectedBlock(int block);
   bool onPlace(User* user, int16_t newblock, int32_t x, int8_t y, int32_t z, int map, int8_t direction);
+  void onStartedDigging(User* user, int8_t status, int32_t x, int8_t y, int32_t z, int map, int8_t direction);
+  void onDigging(User* user, int8_t status, int32_t x, int8_t y, int32_t z, int map, int8_t direction);
+  bool onBroken(User* user, int8_t status, int32_t x, int8_t y, int32_t z, int map,  int8_t direction);
+  bool onInteract(User* user, int32_t x, int8_t y, int32_t z, int map);
 };
 
