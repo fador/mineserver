c87518205ca22df7d90c3993eae5da4138d8c950
Roman Vasiyarov <rvasiyarov@gmail.com>
2011-03-05 00:34:37 +0300

    get rid of CONFIG_PREFIX

--- src/configure.h
+++ src/configure.h
@@ -7,19 +7,11 @@
 #define _CONFIGURE_H
 
 
-// prefix and directory names
-#define CONFIG_PREFIX    "./"
+// directory names
 #define CONFIG_DIR_BIN   "bin/"
 #define CONFIG_DIR_ETC   "bin/"
 #define CONFIG_DIR_LIB   "bin/"
 #define CONFIG_DIR_SHARE "files/"
-
-// paths
-// NOTE: these paths are absolute only when prefix is absolute path!
-#define CONFIG_PATH_BIN   CONFIG_PREFIX""CONFIG_DIR_BIN
-#define CONFIG_PATH_ETC   CONFIG_PREFIX""CONFIG_DIR_ETC
-#define CONFIG_PATH_LIB   CONFIG_PREFIX""CONFIG_DIR_LIB
-#define CONFIG_PATH_SHARE CONFIG_PREFIX""CONFIG_DIR_SHARE
 
 
 #endif // _CONFIGURE_H
