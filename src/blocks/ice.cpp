345b62cf75b6791c095aef46631e7a366e38f54c
Carl Bennett <Admin@Jailout2000.com>
2011-07-14 02:56:20 -0700

    Returned ice.cpp's onBroken() to return true. Ice comes back after broken?? Removing "(char)" from preceding BLOCK_WATER to see if that works.

--- src/blocks/ice.cpp
+++ src/blocks/ice.cpp
@@ -33,10 +33,10 @@
 
 bool BlockIce::onBroken(User* user, int8_t status, int32_t x, int8_t y, int32_t z, int map, int8_t direction)
 {
-  Mineserver::get()->map(map)->setBlock(x, y, z, (char)BLOCK_WATER, direction);
-  Mineserver::get()->map(map)->sendBlockChange(x, y, z, (char)BLOCK_WATER, direction);
+  Mineserver::get()->map(map)->setBlock(x, y, z, BLOCK_WATER, direction);
+  Mineserver::get()->map(map)->sendBlockChange(x, y, z, BLOCK_WATER, direction);
   applyPhysics(user, x, y, z, map);
-  return false;
+  return true;
 }
 
 bool BlockIce::onPlace(User* user, int16_t newblock, int32_t x, int8_t y, int32_t z, int map, int8_t direction)
