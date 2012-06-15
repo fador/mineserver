7dd09525fa64e6b94c71ccdefe03138f05438803
Fador <fador@iki.fi>
2012-06-15 20:12:46 +0300

    Added dummy configure.h for compiling without cmake

---
+++ src/configure.h
@@ -0,0 +1,27 @@
+//
+// defines provided by configuration (cmake)
+//
+
+#ifndef _CONFIGURE_H
+#define _CONFIGURE_H
+
+
+// directory names
+#define CONFIG_DIR_BIN   "."
+#define CONFIG_DIR_ETC   "."
+#define CONFIG_DIR_LIB   "."
+#define CONFIG_DIR_SHARE "."
+#define CONFIG_DIR_DISTSOURCE "."
+
+
+// Version strings
+#define VERSION_MAJOR ""
+#define VERSION_MINOR ""
+#define VERSION_PATCH ""
+#define VERSION_GIT_SHORT ""
+#define VERSION_GIT ""
+#define VERSION_SIMPLE ""
+#define VERSION_COMPLETE ""
+
+
+#endif // _CONFIGURE_H
